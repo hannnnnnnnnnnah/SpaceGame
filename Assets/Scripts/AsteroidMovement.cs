24dab21cf1faec8ff75d4af92afb3cb5901f04ff
Trixie Gardner <dgardner01@mica.edu>
2024-05-02 22:36:52 -0400

    asteroid trail

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class AsteroidMovement : MonoBehaviour
 {
     public Vector3 startPosition;
+    TrailRenderer trailRenderer => GetComponent<TrailRenderer>();
     public float frequency;
     public float magnitude;
+    public float trailLifetime;
     float phase;
 
     // Start is called before the first frame update
@@ -18,6 +20,10 @@ public class AsteroidMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (trailRenderer != null)
+        {
+            trailRenderer.time = trailLifetime;
+        }
         phase += Time.deltaTime;
         Vector3 pos = startPosition;
         float x = magnitude * Mathf.Sin(phase * frequency);
